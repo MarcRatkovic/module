e99315cc5c6170cad7041dde626a77506fe6bc65
Nick Zhu <nz3424@princeton.edu>
2023-01-12 18:10:00 -0500

    setup

--- ExamplePackage/setup.py
+++ ExamplePackage/setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='ExamplePackage',
       version='1.0',
       description='An example package to test before putting in actual modules',
       author='Nick Zhu',
       author_email='nz3424@princeton.edu',
-      url='https://github.com/MarcRatkovic/module',
-      packages=['distutils', 'distutils.command'],
+      url='https://github.com/MarcRatkovic/module/tree/example-package',
+      packages=find_packages(),
      )

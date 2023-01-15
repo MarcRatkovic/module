b7f55678a95655b9993aee930d942a765085353a
Nick Zhu <nz3424@princeton.edu>
2023-01-14 20:07:45 -0500

    test module

--- ExamplePackage/src/example_package_nz3424/hello_world.py
+++ ExamplePackage/src/example_package_nz3424/hello_world.py
@@ -1,5 +1,9 @@
 # hello_world.py
 # ..................................
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+
 def printHW():
     print("Hello, world!")
 

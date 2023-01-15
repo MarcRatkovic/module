22d0ac1d5227a8ddd1c61c17f06b3742217130d8
Nick Zhu <nz3424@princeton.edu>
2023-01-14 19:30:41 -0500

    setup

--- ExamplePackage/setup.py
+++ ExamplePackage/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ExamplePackage_nz3424',
-      version='1.0',
+      version='0.0.2',
       description='An example package to test before putting in actual modules',
       author='Nick Zhu',
       author_email='nz3424@princeton.edu',
